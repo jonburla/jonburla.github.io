88f5303016ebf29c06b5007aab2857a47bb88567
jonburla <Jonathan@Jonathans-MBP.home>
2017-05-29 10:05:27 -0400

    Truce

--- js/script.js
+++ js/script.js
@@ -91,8 +91,8 @@ var deck = ['The Fool', 'The Magician', 'The Priestess', 'The Empress',
 'Creation', 'Transmutation', 'Happiness', 'Satiety', 'Princess of Cups',
 'Prince of Cups','Queen of Cups', 'Knight of Cups',
 
-'Ace of Swords','Peace', 'Sorrow', 'Truce', 'Paradox', 'Science', 'Dexterity',
-'Analysis', 'Princess of Swords', 'Prince of Swords',
+'Ace of Swords','Peace', 'Truce', 'Paradox', 'Science', 'Dexterity', 'Interference',
+'Analysis', 'Trupian Logic', 'Princess of Swords', 'Prince of Swords',
 'Queen of Swords', 'Knight of Swords',
 
 'Ace of Disks','Change', 'Works', 'Power', 'Anxiety', 'Success', 'Perseverance',
